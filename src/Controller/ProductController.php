b333d3443d141fa875760473fcb2cc37b8ce574c
{Meline} <{meline.pischedda@gmail.com}>
2023-01-11 23:52:34 +0100

    Filter Products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\SearchType;
+use App\Classe\Search;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -20,13 +23,22 @@ class ProductController extends AbstractController
     }
 
     #[Route('/nos-produits', name: 'app_products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $search = new Search;
+        $form = $this->createForm(SearchType::class, $search);
 
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
